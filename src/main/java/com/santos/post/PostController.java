ef89368cfe5098e7f683aaf241d6cc22c71272d3
Desuu1414 <sherwin@gmail.com>
2025-05-16 00:08:53 +0800

    new

--- src/main/java/com/santos/post/PostController.java
+++ src/main/java/com/santos/post/PostController.java
@@ -8,6 +8,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
+@CrossOrigin(origins = {"http://localhost:5173", "https://finals-ui.onrender.com"})
 @RequestMapping("/santos/posts")
 public class PostController {
 
